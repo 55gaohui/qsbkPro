9ad057267419be1b15d7f7004fe3a2bee5ea4e35
gaohui <853544904@qq.com>
2019-08-22 17:51:23 +0800

    前后端交互

--- application/api/behavior/CORS.php
+++ application/api/behavior/CORS.php
@@ -1,8 +1,8 @@
 <?php
 
-
 namespace app\api\behavior;
-use think\Request;
+
+use think\Response;
 
 class CORS
 {
@@ -14,6 +14,5 @@ class CORS
         if (request()->isOptions()) {
             exit();
         }
-
     }
 }
