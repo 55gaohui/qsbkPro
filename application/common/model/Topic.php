ab1c44f10638502fe6b40cd6e0588b3b81b949e4
gaohui <853544904@qq.com>
2019-08-27 17:44:27 +0800

    topic 获取文章 withOut

--- application/common/model/Topic.php
+++ application/common/model/Topic.php
@@ -15,19 +15,38 @@ class Topic extends Model
     public function post(){
         return $this->belongsToMany('Post','topic_post');
     }
+    // 关联顶数
+    public function Ding(){
+        return $this->hasMany('Support')->where('type',0);
+    }
     // 关联今日文章
     public function todaypost(){
         return $this->belongsToMany('Post','topic_post')->whereTime('post.create_time', 'today');
     }
     //获取指定话题下的文章（分页）
     public function getPost(){
-        $params = request()->param();
-        $posts = self::get($params['id'])->post()->with(['user'=>function($query){
-            return $query->field('id,username,userpic');
-        },'images'=>function($query){
-            return $query->field('url');
-        },'share'])->page($params['page'],10)->select();
-        return $posts;
+        // 获取所有参数
+        $param = request()->param();
+        // 当前用户id
+        $userId = request()->userId ? request()->userId : 0;
+        $posts = self::get($param['id'])->post()->page($param['page'],10)->select();
+        $arr = [];
+        for ($i=0; $i < count($posts); $i++) {
+            $arr[] = \app\common\model\Post::with([
+                'user'=>function($query) use($userId){
+                    return $query->field('id,username,userpic')->with([
+                        'fens'=>function($query) use($userId){
+                            return $query->where('user_id',$userId)->hidden(['password']);
+                        },'userinfo'
+                    ]);
+                },'images'=>function($query){
+                    return $query->field('url');
+                },'share',
+                'support'=>function($query) use($userId){
+                    return $query->where('user_id',$userId);
+                }])->withCount(['Ding','Cai','comment'])->get($posts[$i]->id)->toArray();
+        }
+        return $arr;
     }
 
     //标题搜索话题
